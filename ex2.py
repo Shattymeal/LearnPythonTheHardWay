39b52d5e848b165ae111791c2d782d39e05297ec
Neil Shetty <Neil.shetty@gmail.com>
2013-02-28 14:49:09 -0800

    Initial Data Commit of exercies

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# A comment, this is so you can read your program later
+# A comment allows you to use plain english to describe your source code
+# Anything after a # is ignored by python
+
+print "I could have code like this" # and anything to the right of the octo be ignored
+
+#you can also use a comment to disable out a piece of your code:
+# print "this won't run"
+
+print "this will run."
---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = 'Neil Shetty'
+my_age = 24
+my_height = 74 #inches
+my_weight = 190 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "he's %d pounds heavy." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
